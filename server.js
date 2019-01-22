e09164aa79af84a996b976813fb931a4cb5abb8d
Phranklin <franklin873@live.com>
2019-01-22 11:17:29 +0530

    Updated with css

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+//const path = require('path');
 
 // create express app
 const app = express();
